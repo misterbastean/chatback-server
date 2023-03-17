acebd740852933943d81f0d5925972d559355f6d
Josh Bastean <joshua.bastean@endava.com>
2023-03-17 11:59:59 -0400

    Add rate limiting

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+
 const routes = require("./routes");
 
 // Express setup
 const app = express();
 app.use(express.json());
+
+// Rate limiting
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use("/api/v1/rooms/:roomCode/checkRoomExists", apiLimiter);
 
 // Routes
 app.use("/api/v1", routes);
